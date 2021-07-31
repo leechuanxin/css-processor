4c8585918dccbfc907903fa512cbd021b6d45fe3
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-01 04:08:55 +0800

    feat: processor options.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { readFile, writeFile } from 'fs';
 import replaceColors from './styles.js';
 
-const filename = process.argv[2];
+const COLOR_CHANGE_TYPE = process.argv[2];
+const FILE_NAME = process.argv[3];
 
 /**
  * Process content and write new content back to original file
@@ -18,22 +19,26 @@ const handleFileRead = (readErr, content) => {
   // split each individual line
   const lines = content.split('\n');
   // replace (any) colors in each line
-  for (let i = 0; i < lines.length; i += 1) {
-    lines[i] = replaceColors(lines[i]);
-  }
-
-  // combine lines to newContent
-  const newContent = lines.join('\n');
-
-  // Write processed content back to the file, replacing old content
-  writeFile(filename, newContent, (writeErr) => {
-    // Catch writing error if any
-    if (writeErr) {
-      console.log('error writing', newContent, writeErr);
-      return;
+  if (COLOR_CHANGE_TYPE === 'rgbtohex' || COLOR_CHANGE_TYPE === 'hextorgb') {
+    for (let i = 0; i < lines.length; i += 1) {
+      lines[i] = replaceColors(lines[i], COLOR_CHANGE_TYPE);
     }
-    console.log('success!');
-  });
+
+    // combine lines to newContent
+    const newContent = lines.join('\n');
+
+    // Write processed content back to the file, replacing old content
+    writeFile(`new_${FILE_NAME}`, newContent, (writeErr) => {
+      // Catch writing error if any
+      if (writeErr) {
+        console.log('error writing', newContent, writeErr);
+        return;
+      }
+      console.log('success!');
+    });
+  } else {
+    console.log('Please enter a valid value for how you would like the colors in the stylesheet to change.');
+  }
 };
 
-readFile(filename, 'utf-8', handleFileRead);
+readFile(FILE_NAME, 'utf-8', handleFileRead);
--- styles.js
+++ styles.js
@@ -1,20 +1,38 @@
-import { hexToRgb } from './conversions.js';
+import { hexToRgb, rgbToHex } from './conversions.js';
 
 /**
  * A function that returns the styles in the stylesheet in an array, with colors replaced
  * @param  text {string} a CSS line, preferably with a property-value pair
  * @return {string}  the CSS line, with color values (if any) replaced
  */
-const replaceColors = (text) => {
+const replaceColors = (text, type) => {
   let line = text;
-  // hex color matching
-  // matches "#[any value from 0 - f, 3 characters to 6 characters]"
-  const hexColorArr = line.match(/#[a-f0-9]{3,6}/g);
-  // using .match, hexColorArr will be an array if there is length > 0
-  // otherwise, it will be null. thus, no need for a length check
-  if (Array.isArray(hexColorArr)) {
-    for (let i = 0; i < hexColorArr.length; i += 1) {
-      line = line.replace(hexColorArr[i], hexToRgb(hexColorArr[i]));
+
+  if (type === 'hextorgb') {
+    // hex color matching
+    // matches "#[any value from 0 - f, 3 characters to 6 characters]"
+    const hexColorArr = line.match(/#[a-f0-9]{3,6}/g);
+    // using .match, hexColorArr will be an array if there is length > 0
+    // otherwise, it will be null. thus, no need for a length check
+    if (Array.isArray(hexColorArr)) {
+      for (let i = 0; i < hexColorArr.length; i += 1) {
+        line = line.replace(hexColorArr[i], hexToRgb(hexColorArr[i]));
+      }
+    }
+  }
+  // input validation already handled in index.js
+  // thus, we assume else-case refers to rgbtohex
+  else {
+    // rgb color matching
+    // matches "rgb([1 to 3 digit number], [1 to 3 digit number], [1 to 3 digit number])",
+    // including any spaces between the characters
+    const rgbColorArr = line.match(/(rgb)\(\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*\d{1,3}\s*\)/g);
+    // using .match, rgbColorArr will be an array if there is length > 0
+    // otherwise, it will be null. thus, no need for a length check
+    if (Array.isArray(rgbColorArr)) {
+      for (let i = 0; i < rgbColorArr.length; i += 1) {
+        line = line.replace(rgbColorArr[i], rgbToHex(rgbColorArr[i]));
+      }
     }
   }
 
